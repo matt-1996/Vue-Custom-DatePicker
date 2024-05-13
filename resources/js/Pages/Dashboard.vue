32ec2d256012e2cc86207e65e8a1de6cfa89056a
matinnasirlahijani <matinnasirlahijani@gmail.com>
2024-05-13 22:09:09 +0330

    Rooms Card Added

--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -1,16 +1,32 @@
 <template>
     <AppLayout title="Dashboard">
         <template #header>
-            <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
+            <h2 class="font-semibold  text-xl text-gray-200 dark:text-gray-800 leading-tight">
                 Dashboard
             </h2>
         </template>
         <div class="py-12">
-            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-                    <JalaliCalendar class="w-full" />
+            <div class="max-w-7xl mx-auto overflow-hidden grid grid-cols-4 gap-4 sm:px-6 lg:px-8">
+                <div v-for="room in rooms" :key="room.id">
+                    <v-card class="mx-auto my-8" elevation="16" max-width="344">
+                        <v-card-item>
+                            <v-card-title >
+                               <Link :href="route('room.show' , room.id)">{{ room.title }}</Link>
+                            </v-card-title>
+
+                            <v-card-subtitle>
+                                {{ room?.user?.name }}
+                            </v-card-subtitle>
+                        </v-card-item>
+
+                        <v-card-text>
+                            {{ room.description }}
+                        </v-card-text>
+                    </v-card>
+                </div>
             </div>
         </div>
-            <!-- <div class="max-w-2xl mx-auto py-10">
+        <!-- <div class="max-w-2xl mx-auto py-10">
                 <JalaliCalendar class="w-full" />
             </div> -->
     </AppLayout>
@@ -18,19 +34,10 @@
 
 <script setup>
 import AppLayout from '@/Layouts/AppLayout.vue';
-import { ref } from 'vue'
+import { Link } from '@inertiajs/vue3'
 import JalaliCalendar from '@/Components/JalaliCalendar/JalaliCalendar.vue';
-const msg = ref('Hello World!')
-import moment from 'moment';
-const newDays = ref()
-const ftow = ref(3)
 
-const d = new Date()
-const today = new Intl.DateTimeFormat('en-US-u-ca-persian', { year: 'numeric', month: '2-digit', day: '2-digit' }).format(d).split(/(\s+)/)
-var m = moment(today[0]).year();
+const props = defineProps({ rooms: Object })
 
-console.log(m)
-
-const selectedDates = ref([])
 
 </script>
